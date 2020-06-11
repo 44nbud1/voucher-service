b3beeefd54dba91e4ac443b0bd98a0b2219942c5
Aan Budi Setiawan <setiawanaa@dnid0695l01.local>
2020-06-11 12:38:35 +0700

    rabbit dikeluarkan

--- src/main/java/com/danapprentech/debrief2/voucherservice/rabbit/model/UpdateQtyConsumer.java
+++ src/main/java/com/danapprentech/debrief2/voucherservice/rabbit/model/UpdateQtyConsumer.java
@@ -1,16 +1,16 @@
-package com.danapprentech.debrief2.voucherservice.rabbit.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class UpdateQtyConsumer implements Serializable
-{
-    private Long idVoucher;
-    private Integer qty;
-}
+//package com.danapprentech.debrief2.voucherservice.rabbit.model;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.io.Serializable;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class UpdateQtyConsumer implements Serializable
+//{
+//    private Long idVoucher;
+//    private Integer qty;
+//}
